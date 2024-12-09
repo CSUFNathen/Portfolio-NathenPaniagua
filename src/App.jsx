4b1426ad4c107c631123052a14be856fc572db5f
CSUFNathen <NathenPaniagua@csu.fullerton.edu>
2024-12-08 19:18:19 -0800

    Update Website

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills"
 import Contact from "./sections/Contact/Contact"
 import Footer from "./sections/Footer/Footer"
+import AboutMe from "./sections/About Me/AboutMe";
 
 function App() {
   return (
     <>
       <Hero />
+      <AboutMe /> 
       <Projects />
       <Skills />
       <Contact />
