606f27924f8ec3252f24bc7a65b26ce2ef0f18b3
CSUFNathen <143219507+CSUFNathen@users.noreply.github.com>
2025-11-17 18:25:24 -0800

    Revise About Me section for updated information
    Updated personal introduction and interests in About Me section.

--- src/sections/About Me/AboutMe.jsx
+++ src/sections/About Me/AboutMe.jsx
@@ -8,12 +8,9 @@ function AboutMe() {
       <h1 className="sectionTitle">About Me</h1>
       
       <p className={styles.aboutText}>
-        Hello, my name is Nathen Paniagua. I’m currently a second-year Computer Science major at California State University Fullerton, with plans to graduate in Spring 2027. 
-        My goal is to become a software engineer or web developer. My interests in programming began when I was younger through video game development, which led me to explore 
-        game design extensively. During high school, I took a game design electives that introduced me to various tools, from Scratch to Unity, and even virtual reality. 
-        This hands-on experience deepened my interest in technology and programming. As I approached college, I chose to pursue Computer Science because it aligns with 
-        my interest in technology and programming, but on a broader scale. While game design remains a hobby of mine, I’m excited about the opportunities in software 
-        engineering, where I can develop applications that impact a wide range of users. Thank you for visiting my website and learning about my journey so far.
+        Hello, my name is Nathen Paniagua. I’m currently a third-year Computer Science major with a minor in Data Science at California State University Fullerton, with plans to graduate in Spring 2027. 
+        My goal is to become a software engineer or Data Scientist. My interest in computer science really started when I got into playing and creating video games, I even took video game design courses 
+        in high school, which pulled me in even more. Outside of tech, I enjoy playing video games, watching and playing sports, and just staying active overall.
       </p>
     </section>
   );
